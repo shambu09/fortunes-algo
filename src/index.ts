f94bc2d08a9ef219dea09c9800d76840a717cf37
Shambu Valasang <shambuqwerty@gmail.com>
2023-07-14 03:31:15 +0530

    Added sample points for testing

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import PriorityQueue from "./PriorityQueue";
 import VEdge from "./VEdge";
 import VPoint from "./VPoint";
 import Voronoi from "./Voronoi";
+import testPoints from "./samplePoints";
 
-export { PriorityQueue, VEdge, VPoint, Voronoi };
+export { PriorityQueue, VEdge, VPoint, Voronoi, testPoints };
